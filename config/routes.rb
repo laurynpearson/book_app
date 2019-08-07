6e6d9c38faddda3b569dd6d7428284d46ac4436c
Lauryn <laurynpearson@gmail.com>
2019-08-07 17:09:21 -0500

    added id to index.json

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
     get "/books" => "books#index"
     get "/books/:id" => "books#show"
     post "/books/" => "books#create"
-    post "/books/" => "books#create"
     patch "/books/:id" => "books#update"
     delete "/books/:id" => "books#destroy"
   end
