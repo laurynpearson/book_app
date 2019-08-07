d52421a776ac7d8c307d02c4c64d27139ac195ce
Lauryn <laurynpearson@gmail.com>
2019-08-06 20:46:31 -0500

    make create route/action

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -7,4 +7,12 @@ class Api::BooksController < ApplicationController
     @book = Book.find_by(id: params[:id])
     render 'show.json.jb'
   end
+  def create
+    @book = Book.new(
+      title: params[:title],
+      pages: params[:pages]
+      )
+    @book.save
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/books" => "books#index"
     get "/books/:id" => "books#show"
+    post "/books/" => "books#create"
   end
 end
